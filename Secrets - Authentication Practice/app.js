53880c6d231dbcc3f1f9d594039c3da057ef2528
Vikas Bhandari <70466756+Vikas031@users.noreply.github.com>
2022-08-07 19:57:22 +0530

    Level -1 Authentication

---
+++ Secrets - Authentication Practice/app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+const express=require('express');
+const ejs=require('ejs');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const app=express();
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/SecretDB');
+}
+//database
+main().catch(err => console.log(err));
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//database schema
+const userSchema=mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const User=mongoose.model('user',userSchema);
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+app.get('/register',(req,res)=>{
+    res.render('register');
+})
+
+app.post('/register',(req,res)=>{
+    console.log(req.body)
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+
+    newUser.save((err)=>{
+        if(!err)
+        res.render('secrets');
+        else
+        res.send(err);
+    })
+})
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username},(err,foundUser)=>{
+        if(err)
+        console.log(err);
+        else{
+            if(foundUser){
+                if(foundUser.password==password)
+                res.render("secrets");
+            }
+        }
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log("App running in port 3000 !")
+})
